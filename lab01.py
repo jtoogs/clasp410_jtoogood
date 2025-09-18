ac2f7cc053291b2137c4506ad48cad75f336f07e
jtoogs <jtoogood@XPS-JT>
2025-09-18 11:17:11 -0400

    end of 9.18 lecture updates to lab01

--- lab01.py
+++ lab01.py
@@ -13,12 +13,11 @@ HI SARAH :) TO REPRODUCE THE VALUES AND PLOTS IN MY REPORT, DO THIS:
 # import packages 
 import numpy as np 
 import matplotlib.pyplot as plt
-import matplot
 
 # declare physical constants
 sigma = 5.67E-8 # Stefan-Boltzmann constant (W/m2/K-4)
 
-def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350.0, debug=false)
+def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350.0, debug=False):
     '''
     String of doc 
     '''
@@ -30,17 +29,30 @@ def n_layer_atmos(nlayers, epsilon=1, albedo=0.33, s0=1350.0, debug=false)
     # Populate based on our model:
     for i in range(nlayers+1):
         for j in range(nlayers+1):
-            A[i, j] = # What math should go here?
+            if i==j: 
+                A[i, j] = -2+1 * (j==0) #diagonal 
+            else:
+                A[i,j] = epsilon**(i>0) * (1-epsilon)**(np.abs(j-i)-1)
     
-    b = # What should go here?
+    b[0] = 0.25 * s0 * (1-albedo) # longwave is only the first E_in
 
     # Invert matrix:
     Ainv = np.linalg.inv(A)
     # Get solution:
     fluxes = np.matmul(Ainv, b) # Note our use of matrix multiplication!
 
+    # turn fluxes into temperatures
+    temps = (fluxes * (1/sigma))**(1/4) # this is not right 
+
+    # return temperatures to caller 
+    return temps 
+
+    # verify 
+    print(temps)
+
     if debug:
         print(f'A[i={i},j={j}] = {A[i, j]}')
+        print(f'{A}')
 
 
 
