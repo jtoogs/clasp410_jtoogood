1a8ebaab86be7dc13ec1fc123a4b5b4af38b458e
jtoogs <jtoogood@XPS-JT>
2025-10-06 02:29:36 -0400

    added lab02 scaffold

---
+++ lab02.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env/python3
+'''
+Lab #2: Population Control
+Julian Toogood
+
+This file solves the population control problem for Lab 02 and all subparts. 
+
+Hi Sarah :) 
+To reproduce the values and plots in my report, do this: 
+- For question 1, run the function question_1() with no inputs.
+- For question 2, run the function question_2() with no inputs.
+- For question 3, run the function question_3() with no inputs.
+
+'''
+
+# import packages 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# turn on interactive plotting, set stylesheet
+plt.ion()
+plt.style.use('seaborn-v0_8-poster')
+
+
+def question_1(debug=False):
+    '''
+    Run this code to reproduce all results for question 1.
+
+    Parameters
+    ----------
+    debug : boolean, default=False
+        Activates intermediate print steps to check for incorrect values
+
+    Returns: none
+    '''
+
+def question_2(debug=False):
+    '''
+    Run this code to reproduce all results for question 2.
+
+    Parameters
+    ----------
+    debug : boolean, default=False
+        Activates intermediate print steps to check for incorrect values
+
+    Returns: none
+    '''
+
+def question_2(debug=False):
+    '''
+    Run this code to reproduce all results for question 2.
+
+    Parameters
+    ----------
+    debug : boolean, default=False
+        Activates intermediate print steps to check for incorrect values
+
+    Returns: none
+    '''
+
+
+
+print('Question 1:')
+question_1()
+
+print('Question 2:')
+question_2()
+
+print('Question 3:')
+question_3()
