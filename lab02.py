d2287a166f768e6ec7ba5edd645c55c0497b7743
jtoogs <jtoogood@XPS-JT>
2025-10-06 16:53:17 -0400

    all modeling functions working correctly

--- lab02.py
+++ lab02.py
@@ -53,25 +53,68 @@ def dNdt_comp(t, N, a=1, b=2, c=1, d=3):
 
     return dN1dt, dN2dt
 
-def euler_solve(func, N1_init=.5, N2_init=.5, dT=.1, t_final=100.0):
+def dNdt_pp(t, N, a=1, b=2, c=1, d=3):
     '''
-    <Your good docstring here>
+    This function calculates the Lotka-Volterra predator/prey equations for
+    two species. Given normalized populations, `N1` and `N2`, as well as the
+    four coefficients representing population growth and decline,
+    calculate the time derivatives dN_1/dt and dN_2/dt and return to the
+    caller.
+    This function accepts `t`, or time, as an input parameter to be
+    compliant with Scipy's ODE solver. However, it is not used in this
+    function.
+
+    Parameters
+    ----------
+    t : float
+        The current time (not used here).
+    N : two-element list
+        The current values of N1 and N2 as a list (e.g., [N1, N2]).
+    a, b, c, d : float, defaults=1, 2, 1, 3
+        The values of the Lotka-Volterra coefficients.
+
+    Returns
+    -------
+    dN1dt, dN2dt : floats
+        The time derivatives of `N1` and `N2`.
+    '''
+
+    # Here, N is a two-element list such that N1=N[0] and N2=N[1]
+    dN1dt = a*N[0] - b*N[0]*N[1]
+    dN2dt = -c*N[1] + d*N[1]*N[0]
+
+    return dN1dt, dN2dt
+
+def euler_solve(func, N1_init=.5, N2_init=.5, dT=.1, t_final=100.0,
+                a=1, b=2, c=1, d=3):
+    '''
+    Solve the Lotka-Volterra competition and predator/prey equations using
+    a first-order-accurate Euler method with a constant forward time step.
 
     Parameters
     ----------
     func : function
         A python function that takes `time`, [`N1`, `N2`] as inputs and
         returns the time derivative of N1 and N2.
-    N1_init : float
-        DESCRIPTION
+    N1_init, N2_init : float, default=0.5
+        Initial conditions for `N1` and `N2`, ranging from (0,1]
+    dT : float, default=10
+        Largest time step allowed in years.
+    t_final : float, default=100
+        Integrate until this value is reached, in years.
+    a, b, c, d : float, default=1, 2, 1, 3
+        Lotka-Volterra coefficient values
     
     Returns
     -------
-    <more good docstring here>
+    time : Numpy array
+        Time elapsed in years.
+    N1, N2 : Numpy arrays
+        Normalized population density solutions.
     '''
 
     ## from lecture example 
-    # configure the problem
+    # configure the problem / preallocate variables 
     time = np.arange(0, t_final, dT)
     N1 = np.zeros(time.size)
     N2 = np.zeros(time.size)
@@ -82,14 +125,14 @@ def euler_solve(func, N1_init=.5, N2_init=.5, dT=.1, t_final=100.0):
 
     # solve for the function values 
     for i in range(time.size-1):
-        dN1[i], dN2[i] = func(i, [N1[i], N2[i]] ) #retrieve derivatives at index i 
+        dN1[i], dN2[i] = func(i, [N1[i], N2[i]], a=a, b=b, c=c, d=d) #retrieve derivatives at index i 
         N1[i+1] = N1[i] + (dT * dN1[i]) #euler method to find next function value 
         N2[i+1] = N2[i] + (dT * dN2[i])
 
     return time, N1, N2 
 
 def solve_rk8(func, N1_init=.5, N2_init=.5, dT=10, t_final=100.0,
-a=1, b=2, c=1, d=3):
+                a=1, b=2, c=1, d=3):
     '''
     Solve the Lotka-Volterra competition and predator/prey equations using
     Scipy's ODE class and the adaptive step 8th order solver.
@@ -99,7 +142,7 @@ a=1, b=2, c=1, d=3):
     func : function
         A python function that takes `time`, [`N1`, `N2`] as inputs and
         returns the time derivative of N1 and N2.
-    N1_init, N2_init : float
+    N1_init, N2_init : float, default=0.5
         Initial conditions for `N1` and `N2`, ranging from (0,1]
     dT : float, default=10
         Largest time step allowed in years.
@@ -127,29 +170,29 @@ a=1, b=2, c=1, d=3):
     # Return values to caller.
     return time, N1, N2
 
-def check_solver(dt=1.0):
+def check_solver(func):
     '''
     Quickly see if the function is working properly. 
     Also serves as scaffolding/sandbox for preparing question functions  
 
-    Parameters
-    ---------
-    dt : float, defaults to 1.0
-        Set the time step for the solver.
+    Parameters: 
+    ----------
+    func : function
+        A python function that takes `time`, [`N1`, `N2`] as inputs and
+        returns the time derivative of N1 and N2.
+    Returns: none 
     '''
 
-    t = np.arange(0, 300., 0.5)
-
-    # Obtain Euler solver numerical solution.
-    etime, eN1, eN2 = euler_solve(dNdt_comp)
-    rk8time, rk8N1, rk8N2 = solve_rk8(dNdt_comp)
+    # Obtain solver solutions.
+    etime, eN1, eN2 = euler_solve(func)
+    rk8time, rk8N1, rk8N2 = solve_rk8(func)
 
     # Make a beautiful plot to illustrate how the numerical solution
     # performs.
     fig, ax = plt.subplots(1, 1, figsize=[10.24,  5.91])
     # Plot lines we want to show:
-    ax.plot(etime, eN1, 'o--', label=f'N1 Euler Solution for $\Delta t={dt}s$')
-    ax.plot(etime, eN2, 'o--', label=f'N2 Euler Solution for $\Delta t={dt}s$')
+    ax.plot(etime, eN1, 'o--', label=f'N1 Euler Solution for $\Delta t=(dt)s$')
+    ax.plot(etime, eN2, 'o--', label=f'N2 Euler Solution for $\Delta t=(dt)s$')
     ax.plot(rk8time, rk8N1, 'o--', label=f'N1 RK8 Solution')
     ax.plot(rk8time, rk8N2, 'o--', label=f'N2 RK8 Solution')
 
@@ -172,6 +215,48 @@ def question_1(debug=False):
 
     Returns: none
     '''
+
+    # Obtain solver solutions.
+    etime_comp, eN1_comp, eN2_comp = euler_solve(dNdt_comp,N1_init=0.3,N2_init=0.6,dT=1.0)
+    rk8time_comp, rk8N1_comp, rk8N2_comp = solve_rk8(dNdt_comp,N1_init=0.3,N2_init=0.6,dT=1.0)
+    etime_pp, eN1_pp, eN2_pp = euler_solve(dNdt_pp,N1_init=0.3,N2_init=0.6,dT=0.05)
+    rk8time_pp, rk8N1_pp, rk8N2_pp = solve_rk8(dNdt_pp,N1_init=0.3,N2_init=0.6,dT=0.05)
+
+    # Make a beautiful plot to illustrate how the numerical solution
+    # performs.
+    fig, [ax1, ax2] = plt.subplots(1, 2, figsize=[12,6])
+    # Plot lines we want to show:
+    ax1.plot(etime_comp, eN1_comp, '-', color='b', label=f'N1 Euler')
+    ax1.plot(etime_comp, eN2_comp, '-', color='orangered', label=f'N2 Euler')
+    ax1.plot(rk8time_comp, rk8N1_comp, ':', color='b', label=f'N1 RK8')
+    ax1.plot(rk8time_comp, rk8N2_comp, ':', color='orangered', label=f'N2 RK8')
+
+    ax1.legend(loc='upper left')
+    ax1.set_xlabel('Time $(years)$')
+    ax1.set_ylabel('Population/Carrying Cap.')
+    ax1.set_title('Lokta-Volterra Competition Model')
+    ax1.grid(True)
+
+    ax2.plot(etime_pp, eN1_pp, '-', color='b', label=f'N1 (Prey) Euler')
+    ax2.plot(etime_pp, eN2_pp, '-', color='orangered', label=f'N2 (Predator) Euler')
+    ax2.plot(rk8time_pp, rk8N1_pp, ':', color='b', label=f'N1 (Prey) RK8')
+    ax2.plot(rk8time_pp, rk8N2_pp, ':', color='orangered', label=f'N2 (Predator) RK8')
+
+    ax2.legend(loc='upper left')
+    ax2.set_xlabel('Time $(years)$')
+    ax2.set_ylabel('Population/Carrying Cap.')
+    ax2.set_title('Lokta-Volterra Predator-Prey Model')
+    ax2.grid(True)
+
+    plt.figtext(0.5, 0.01, 'Coefficients: a=1, b=2, c=1, d=3', 
+            ha='center', va='bottom', fontsize=12)
+    
+    fig.tight_layout()
+
+    print("We verify our code is working by reproducing the figure from the lab instructions.")
+    print("Our figure matches the one from the lab instructions, so our code appears to be " \
+    "working correctly.")
+    
 
 def question_2(debug=False):
     '''
@@ -197,6 +282,9 @@ def question_3(debug=False):
     Returns: none
     '''
 
+#check solver
+# check_solver(dNdt_comp)
+# check_solver(dNdt_pp)
 
 print('Question 1:')
 question_1()
