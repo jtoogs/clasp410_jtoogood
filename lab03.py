8d86c6bc6b255df0c95995e0bf98cb4b6f2fe380
jtoogs <jtoogood@XPS-JT>
2025-10-23 12:45:09 -0400

    solver works

--- lab03.py
+++ lab03.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env/python3
 '''
-Lab #3: eat Diffusion & Permafrost
+Lab #3: Heat Diffusion & Permafrost
 Julian Toogood
 
 This file solves the 1D diffusion equation for Lab 03 and all subparts. 
@@ -17,6 +17,7 @@ Running this script will also call these functions in order.
 # import packages 
 import numpy as np 
 import matplotlib.pyplot as plt
+from tabulate import tabulate #remove for submission
 
 # turn on interactive plotting, set stylesheet
 plt.ion()
@@ -60,7 +61,7 @@ def solve_heat(xstop=1., tstop=0.2, dx=0.02, dt=0.0002, c2=1, initial=None, lowe
 
     Comments
     ----------
-    Lab04 uses dirichlet boundary conditions but the UB value changes as a function of time
+    Lab03 uses dirichlet boundary conditions but the UB value changes as a function of time
     '''
 
     # Check our stability criterion:
@@ -69,8 +70,8 @@ def solve_heat(xstop=1., tstop=0.2, dx=0.02, dt=0.0002, c2=1, initial=None, lowe
         raise ValueError(f'DANGER: dt={dt} > dt_max={dt_max}.')
     
     # Get grid sizes:
-    N = int(tstop / dt)
-    M = int(xstop / dx)
+    N = int(tstop / dt) + 1
+    M = int(xstop / dx) + 1
 
     # Set up space and time grid:
     t = np.linspace(0, tstop, N)
@@ -78,7 +79,7 @@ def solve_heat(xstop=1., tstop=0.2, dx=0.02, dt=0.0002, c2=1, initial=None, lowe
 
     # Create solution matrix; set initial conditions
     U = np.zeros([M, N])
-    # U[:, 0] = fx
+    # U[:, 0] = initial
     U[:, 0] = 4*x - 4*x**2   #hardcoded override for hw       
 
     # Get our "r" coeff:
@@ -135,16 +136,16 @@ def question_1():
     Returns: none
     '''
     # Get solution using your solver:
-    time, x, heat = solve_heat()
+    time, x, heat = solve_heat(xstop=1., tstop=0.2, dx=0.2, dt=0.02, c2=1, initial=None, upperbound=0,lowerbound=0)
 
     # Create a figure/axes object
     fig, axes = plt.subplots(1, 1)
 
     # Create a color map and add a color bar.
-    map = axes.pcolor(time, x, heat, cmap='seismic') #, vmin=-25, vmax=25
+    map = axes.pcolor(time, x, heat, cmap='hot') #, vmin=-25, vmax=25
     plt.colorbar(map, ax=axes, label='Temperature ($C$)')
 
-    print(heat)
+    print(tabulate(heat))
 
 def question_2():
     '''
